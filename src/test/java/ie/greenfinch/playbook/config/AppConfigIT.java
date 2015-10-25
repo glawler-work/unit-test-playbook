360e5530d6c475294ee843d8e924c5d51ba73b79
unknown <gary.lawler@gmail.com>
2015-10-25 13:32:33 +0000

    use ConfigurableApplicationContext

--- src/test/java/ie/greenfinch/playbook/config/AppConfigIT.java
+++ src/test/java/ie/greenfinch/playbook/config/AppConfigIT.java
@@ -1,17 +1,13 @@
 package ie.greenfinch.playbook.config;
 
-import ie.greenfinch.playbook.model.ModelClass;
-import ie.greenfinch.playbook.service.TestMeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
-import org.springframework.core.env.Environment;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -19,10 +15,10 @@ import static org.hamcrest.MatcherAssert.assertThat;
 public class AppConfigIT {
 
     @Autowired
-    private ApplicationContext context;
+    private ConfigurableApplicationContext context;
 
     @Test
     public void contextCreatedSuccessfully() throws Exception {
-        assertThat(context.getApplicationName(), is(notNullValue()));
+        assertThat(context.isActive(), is(true));
     }
 }
